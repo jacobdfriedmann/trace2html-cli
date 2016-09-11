c3dc8506c7ce5509b82147b90bc900eec63d4d05
Jacob Friedmann <jacobdfriedmann@gmail.com>
2016-09-11 17:52:44 -0400

    attempt to fix path error

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #! /usr/bin/env node
 var userArgs = process.argv.slice(2);
 var exec = require('child_process').exec;
+var getModuleDirectory = require('./getModuleDirectory');
 
 exec(
-  __dirname + '/catapult/tracing/bin/trace2html ' + userArgs.join(' '),
+  getModuleDirectory() + '/catapult/tracing/bin/trace2html ' + userArgs.join(' '),
   function(err, stdout, stderr) {
     console.log(err);
     console.log(stdout);
